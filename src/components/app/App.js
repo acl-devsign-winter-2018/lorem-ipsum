e275d8e05c21248b45a5d05c716a3c3d5beb5d0b
Ivan Limongan <ivan.limongan@gmail.com>
2018-01-23 17:31:25 -0800

    webpack proper

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,7 +3,7 @@ import html from './app.html';
 import './app.css';
 import Header from '../header/Header';
 import Home from '../home/Home';
-// import Footer from '../footer/Footer.js';
+import Footer from '../footer/Footer.js';
 
 import { removeChildren } from '../dom';
 
@@ -33,7 +33,7 @@ export default class App {
     const dom = template.clone();   
       
     dom.querySelector('header').appendChild(new Header().render());
-    // dom.querySelector('footer').appendChild(new Footer().render());
+    dom.querySelector('footer').appendChild(new Footer().render());
     this.main = dom.querySelector('main');
     this.setPage();
   
